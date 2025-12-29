d8d907a95f7b40d86cf0574a831c6f40949b621e
nawazashraf <ashrafnawaz843@gmail.com>
2025-12-30 01:23:15 +0530

    Update User.js

--- Server/models/User.js
+++ Server/models/User.js
@@ -5,8 +5,12 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   organizationName: { type: String, required: true },
-  department: { type: String, default: "General" }, 
+  department: { type: String, default: "General" },
   branch: { type: String, default: "Main" },
+  hasCompletedOnboarding: {
+    type: Boolean,
+    default: false
+  },
   createdAt: { type: Date, default: Date.now }
 });
 
