15db4e9ec780af4fc25be6bcfb91df32cc9bfc28
nawazashraf <ashrafnawaz843@gmail.com>
2025-12-29 15:51:42 +0530

    removed extra padding

--- Client/src/layout/MainLayout.tsx
+++ Client/src/layout/MainLayout.tsx
@@ -10,7 +10,7 @@ const MainLayout = () => {
       <Sidebar collapsed={collapsed} setCollapsed={setCollapsed} />
 
       <main
-        className={`min-h-screen w-full bg-gray-100 p-6 transition-all duration-300
+        className={`min-h-screen w-full bg-gray-100 transition-all duration-300
         ${collapsed ? "ml-20" : "ml-64"}`}
       >
         <Outlet />
