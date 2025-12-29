17b42b6926428238df0f42c274f305e2d772c10e
nawazashraf <ashrafnawaz843@gmail.com>
2025-12-30 01:25:17 +0530

    Update User.js

--- Server/models/User.js
+++ Server/models/User.js
@@ -1,17 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
+  clerkUserId: { type: String, required: true, unique: true },
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
   organizationName: { type: String, required: true },
   department: { type: String, default: "General" },
   branch: { type: String, default: "Main" },
   hasCompletedOnboarding: {
     type: Boolean,
-    default: false
+    default: false,
   },
-  createdAt: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now },
 });
-
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
